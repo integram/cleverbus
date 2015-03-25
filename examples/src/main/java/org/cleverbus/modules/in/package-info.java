e3e75b704e0436e5b30c2c25ef427300da50f3e2
Michal Cervinka <michal.cervinka@cleverlance.com>
2015-03-25 12:58:07 +0100

    Licence change from Apache License 2.0 to GPLv3

--- examples/src/main/java/org/cleverbus/modules/in/package-info.java
+++ examples/src/main/java/org/cleverbus/modules/in/package-info.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * Modules which process input requests from external systems
  * - public interfaces are divided by entity type + there is special interface for communication with Vodafone (VF),
